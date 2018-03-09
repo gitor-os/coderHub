80d519e6235e96bc4bef8a78c33bdac73bbe2949
刘文龙 <1@gitor.org>
2018-03-09 23:52:14 +0800

    测试http 请求

--- pages/questionBank/list/list.js
+++ pages/questionBank/list/list.js
@@ -5,62 +5,74 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    list: [],
+    list2: [
+      {id:2,name:"long"}, {id:3,name:"li"}
+
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    wx.request({
+      url: 'http://localhost:8088/coder-hub',
+      method: "POST",
+      dataType: "json",
+      success: function (r) {
+        console.log("======list2======");
+        console.log(r.data);
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
